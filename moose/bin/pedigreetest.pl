5921a3262808194c78af2fbc6e09be3cd50ac9e5
tomfy <tly2@cornell.edu>
2021-02-06 14:54:43 -0500

    Add option to exclude low quality markers.

--- moose/bin/pedigreetest.pl
+++ moose/bin/pedigreetest.pl
@@ -15,20 +15,23 @@ BEGIN {     # this has to go in Begin block so happens at compile time
   $libdir = abs_path($libdir);	# collapses the bin/../lib to just lib
 }
 use lib $libdir;
-print STDERR "libdir: $libdir \n";
+print STDERR "# libdir: $libdir \n";
 
 use GenotypesSet;
 use Pedigrees;
 use CheckPedigrees;
-# use PedigreeCheck;
 
 my $gtfilename = undef;
 my $pedigree_filename = undef;
-my $delta = 0.5;
+my $delta = 0.25;
+my $max_bad_gt_fraction = 1.0;
+my $min_hw_qual_param = 0.0;
  GetOptions(
 	    'gtsfile|gtfile|genotypesfile=s' => \$gtfilename,
 	    'pedigreefile=s' => \$pedigree_filename,
 	    'delta=f' => \$delta,
+	    'max_bad_gt_fraction=f' => \$max_bad_gt_fraction,
+	    'min_hw_qual=f' => \$min_hw_qual_param,
 	   );
 
 die "No genotypes matrix filename provided.\n" if(!defined $gtfilename);
@@ -38,10 +41,12 @@ my $gtset = GenotypesSet->new({gt_matrix_filename => $gtfilename, delta => $delt
 print STDERR "# GenotypesSet object created.\n";
 
 #print $gtset->as_string(), "\n";
-print STDERR "# ", $gtset->marker_qual_string, "\n";
+print STDERR "# ", $gtset->marker_qual_string(), "\n";
 #exit;
 # print $gtset->gt_set_as_string(), "\n";
-
+if( ($max_bad_gt_fraction < 1.0) or ($min_hs_qual_param > 0.0) ){
+  $gtset->remove_bad_markers($max_bad_gt_fraction, $min_hw_qual_param);
+}
 print STDERR "# Create pedigrees object from file.\n";
 my $pedigrees = Pedigrees->new({pedigree_filename => $pedigree_filename});
 print STDERR "# Pedigrees object created.\n";
@@ -49,5 +54,5 @@ print STDERR "# Pedigrees object created.\n";
 print STDERR "# Create CheckPedigrees object.\n";
 my $check_peds = CheckPedigrees->new({genotypes_set => $gtset, pedigrees => $pedigrees});
 print STDERR "# PedigreeChecks object created.\n";
-print $check_peds->as_string_ns();
+print $check_peds->as_string_Ns();
 #print $check_peds->as_string_zs();
