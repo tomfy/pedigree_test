9d31ebf9a364b0a056f1fbf1b605d95bf5ff1f25
tomfy <tly2@cornell.edu>
2021-03-18 21:17:15 -0400

    sub construct_cleaned_genotypes_object construct new genotypes obj. from existing one, removing low qual. markers.

--- moose/lib/Genotypes.pm
+++ moose/lib/Genotypes.pm
@@ -17,10 +17,10 @@ has id => ( isa => 'Str',
 	  );
 
 has genotypes => ( # e.g. '01003021' i.e. only 0, 1, 2, 3, 4 with no separating characters.
-		       isa => 'Str',
-		       is => 'rw',
-		       required => 1,
-		      );
+		  isa => 'Str',
+		  is => 'rw',
+		  required => 1,
+		 );
 
 has quality_counts => ( # e.g. '1000 700 200 5 3' 4nd and 5th numbers are counts of gts with ambiguous gts.
 		       isa => 'Str',
@@ -30,7 +30,8 @@ has quality_counts => ( # e.g. '1000 700 200 5 3' 4nd and 5th numbers are counts
 
 sub as_string{
   my $self = shift;
-  my $s = $self->id() . "  " . $self->quality_counts() . "  " . $self->genotypes();
+  my $s = $self->id() . "  " . $self->genotypes();
+  return $s;
 }
 
 sub agmr_hgmr{
@@ -55,7 +56,7 @@ sub agmr_hgmr{
   return [$agmr, $hgmr];
 }
 
-sub remove_bad_markers{
+sub remove_bad_markers{ # remove bad markers from THIS object
   my $self = shift;
   my $marker_01s = shift; # array ref, with ids of 'bad' markers replaced with undef.
   my $new_genotypes_string = '';
@@ -69,14 +70,42 @@ sub remove_bad_markers{
     if ($marker_01s->[$i] == 1) { # this marker is Ok
       $new_genotypes_string .= substr($gtstr, $i, 1);
       $new_qual_counts[$gt]++;
-    } else {			# this marker is 'bad'
+    } else {		  # this marker is 'bad'
       # print STDERR "marker $i is bad.\n"; # die;
     }
   }
+  #  my $good_genotypes_obj = Genotypes->new({ genotypes => $new_genotypes_string, 
   $self->genotypes($new_genotypes_string);
   $self->quality_counts(join(" ", @new_qual_counts));
   # print STDERR "new quality counts:  ", join(" ", @new_qual_counts), "\n";
   die if(length $new_genotypes_string  !=  sum(@new_qual_counts));
+}
+
+sub construct_cleaned_genotypes_object{ # construct, from a genotypes obj, a new genotypes obj with only 'good' markers 
+  my $self = shift;
+  my $marker_01s = shift; # array ref, with ids of 'bad' markers replaced with undef.
+  my $new_genotypes_string = '';
+  my @new_qual_counts = (0, 0, 0, 0, 0); # for updated accession gt quality counts
+  my $L = length $self->genotypes();
+  # print STDERR "L: $L  ", scalar @$marker_01s, "\n";
+  die if(scalar @$marker_01s != $L);
+  my $gtstr = $self->genotypes();
+  for my $i (0..$L-1) {
+    my $gt = substr($gtstr, $i, 1);
+    if ($marker_01s->[$i] == 1) { # this marker is Ok
+      $new_genotypes_string .= substr($gtstr, $i, 1);
+      $new_qual_counts[$gt]++;
+    } else {		  # this marker is 'bad'
+      # print STDERR "marker $i is bad.\n"; # die;
+    }
+  }
+  die if(length $new_genotypes_string  !=  sum(@new_qual_counts));
+  my $good_genotypes_obj = Genotypes->new(
+					  { id => $self->id(),
+					    genotypes => $new_genotypes_string,
+					    quality_counts => join(" ", @new_qual_counts)}
+					 );
+  return $good_genotypes_obj;
 }
 
 
