d45ccd6baaf8d1e7c9242adc95ef0280700d04f8
tomfy <tly2@cornell.edu>
2021-05-09 20:28:04 -0400

    subroutines to check acyclicity.

--- moose/lib/Node.pm
+++ moose/lib/Node.pm
@@ -28,13 +28,13 @@ has male_parent => (
 		    default => undef,
 		   );
 
-has offspring => (
+has offspring => ( # space-separated ids of offspring.
 		  isa => 'Str',
 		  is => 'rw',
 		  default => '',
-		  );
+		 );
 
-sub as_newick{
+sub as_newick{ # generate a newick string representing the pedigree of a node.
   my $self = shift;
   my $newick = '';
   my ($mindF, $maxdF, $mindM, $maxdM);
@@ -50,10 +50,10 @@ sub as_newick{
   if (defined $self->male_parent()) {
     (my $nwck, $mindM, $maxdM) =  $self->male_parent()->as_newick();
     $newick .= $nwck;
-       $mindM++; $maxdM++;
+    $mindM++; $maxdM++;
   } else {
     $newick .= 'NA';
-       $mindM = 0; $maxdM = 0;
+    $mindM = 0; $maxdM = 0;
   }
   return ( '(' . $newick . ')' . $self->id, min($mindF, $mindM), max($maxdF, $maxdM) );
 }
@@ -64,6 +64,35 @@ sub add_offspring{
   my $updated_offspring_id_string = $self->offspring() . "$new_offspring_id ";
   $self->offspring($updated_offspring_id_string);
   # print STDERR "offspringstring: ", $self->offspring(), "\n";
+  
 }
+
+sub ancestors_acyclic{
+  # start from node and recursively search (depth first)
+  # ancestors to check for cycles in the directed graph of node and all its
+  # ancestors.
+  my $self = shift;
+  my $descendant_ids = shift;
+  $descendant_ids->{$self->id()} = 1;
+  return (ancestors_acyclic_one_side($self->female_parent(), $descendant_ids)
+	  and
+	  ancestors_acyclic_one_side($self->male_parent(), $descendant_ids) )
+}
+
+sub ancestors_acyclic_one_side{
+  my $parent_node = shift;
+  my $desc_ids = shift;
+  if (defined $parent_node) {
+    if (exists $desc_ids->{$parent_node->id()}) {
+      return 0;
+    } else {
+      my %desc_ids_copy = %{$desc_ids};
+      return $parent_node->ancestors_acyclic(\%desc_ids_copy);
+    }
+  } else {
+    return 1;
+  }
+}
+
 
 1;
